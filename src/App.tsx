fff9453f333b46a28e97f7291c103bbce7a2df21
bakai-taalaibek <bakai.pochta@gmail.com>
2024-10-16 10:43:29 +0600

    Add rays

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import "./App.css";
 
+const customYellow = "bg-yellow-300";
+const customRed = "bg-red-600";
+
 type TProps = {
   children: React.ReactNode;
   isOpposite?: boolean;
@@ -7,22 +10,53 @@ type TProps = {
 
 const StripesContainer = ({ children, isOpposite }: TProps) => (
   <div
-    className={`flex justify-center items-center gap-2 flex-col absolute rotate-45 ${
-      isOpposite ?? "-rotate-45"
+    className={`flex justify-center items-center gap-2 flex-col absolute ${
+      isOpposite ? "-rotate-45" : "rotate-45"
     } `}
   >
     {children}
   </div>
 );
 
-const stripe = <div className="bg-red-600 w-48 h-1.5"></div>;
+const stripe = <div className={`${customRed} w-48 h-1.5`}></div>;
+
+const RayOnStick = () => (
+  <div className={`flex flex-col absolute items-center`}>
+    <div className={`h-[110px] w-0 bg-green-500 relative`}>
+      <div
+        className={`absolute transform translate-y-full -translate-x-1/2 bottom-0 left-1/2 w-0 h-0 
+          border-l-[9px] border-r-[9px] border-t-[32px] 
+          border-l-transparent border-r-transparent border-t-yellow-300`}
+      ></div>
+    </div>
+  </div>
+);
 
 function App() {
   return (
-    <div className="aspect-[3/2] bg-red-600 h-96 flex justify-center items-center">
-      <div className="bg-yellow-300 rounded-full h-56 aspect-square flex justify-center items-center">
-        <div className="bg-red-600 rounded-full h-52 aspect-square flex justify-center items-center">
-          <div className="bg-yellow-300 rounded-full h-48 aspect-square flex justify-center items-center">
+    <div
+      className={`${customRed} aspect-[3/2] h-96 flex justify-center items-center`}
+    >
+      <div className="absolute transform translate-x-1/2">
+        {Array.from(Array(40).keys()).map((_, index) => (
+          <div
+            style={{
+              transform: `rotate(${index * 9}deg)`,
+            }}
+          >
+            <RayOnStick />
+          </div>
+        ))}
+      </div>
+      <div
+        className={`${customYellow} rounded-full h-56 aspect-square flex justify-center items-center`}
+      >
+        <div
+          className={`${customRed} rounded-full h-52 aspect-square flex justify-center items-center`}
+        >
+          <div
+            className={`${customYellow} rounded-full h-48 aspect-square flex justify-center items-center`}
+          >
             <StripesContainer>
               {Array.from(Array(4).keys()).map(() => stripe)}
             </StripesContainer>
